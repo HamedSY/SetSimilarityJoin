5bea52c1eaeb770128e2e19539236834dbe26c87
HamedSY <hamed007.saboor@gmail.com>
2025-07-23 14:55:59 +0200

    Add a guiding comment

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,6 +3,7 @@ object Main extends App {
   // Create a "PPJoinHandler"/"AllPairsHandler" instance
   // Change the threshold if you want
   val ssjAlgorithmHandler = new PPJoinHandler(threshold = 0.7)
-  // Address of the dataset
+  // Address of the dataset(s)
+  // Use "executeRSJoin" if you have two datasets
   executer.executeSelfJoin("data/bms-pos-5p-sample.txt", ssjAlgorithmHandler)
 }
